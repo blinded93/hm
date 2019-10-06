e4ff47a24b632f8ff76cc33369180a1d6ac2a4c3
Kyle Hughes <blinded93@gmail.com>
2019-10-06 12:10:41 -0700

    add list resources

---
+++ db/migrate/20191006190156_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lists do |t|
+      t.string :name
+      t.references :household, null: false, foreign_key: true, index: true
+
+      t.timestamps
+    end
+  end
+end
