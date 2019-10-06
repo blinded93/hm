37dc185a1d473ffd1d9b74d6fcc943d65c3837af
Kyle Hughes <blinded93@gmail.com>
2019-10-06 12:11:02 -0700

    add task resources

---
+++ db/migrate/20191006185825_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.references :household, null: false, foreign_key: true, index: true
+      t.date :due_date
+      t.boolean :completed?, default: false
+
+      t.timestamps
+    end
+  end
+end
