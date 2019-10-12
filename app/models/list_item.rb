fa0ef4a4de4a3e8b4dea5e3115b216ac81d5872b
Kyle Hughes <blinded93@gmail.com>
2019-10-11 22:07:12 -0700

    remove unused validation

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,15 +2,7 @@ class ListItem < ApplicationRecord
   belongs_to :list
   belongs_to :item
 
-  validates :unique?
-
   def item_attributes=(attrs)
     self.item = Item.find_or_create_by(attrs)
-  end
-
-  def unique?
-    if !persisted? && ListItem.find_by(list_id: list_id, item_id: item_id)
-      errors.add(:item_id, 'is already on the list.')
-    end
   end
 end
