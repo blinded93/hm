7458bd91fae54ca7f7a87033347af62efb598831
Kyle Hughes <blinded93@gmail.com>
2019-10-11 20:03:38 -0700

    ad task validations and scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :household
+
+  validates :name, :due_date, presence: true
+
+  default_scope { order(due_date: :desc) }
+  scope :over_due, -> { where('due_date < ?', Date.today) }
+  
+  scope :completed, -> { where(completed?: true) }
+
+  def not_in_past
+    if !due_date.blank? && due_date < Date.today
+      errors.add(:due_date, "can't be in the past.")
+    end
+  end
 end
