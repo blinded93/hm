1c9a5320efd980a614e76a961b7d1f39ad921dc5
Kyle Hughes <blinded93@gmail.com>
2019-10-11 20:03:20 -0700

    add list associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,10 @@
 class List < ApplicationRecord
   belongs_to :household
+
+  has_many :list_items, :dependent: :destroy
+  has_many :items, through: :list_items
+
+  def size
+    items.count
+  end
 end
