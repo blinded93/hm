98b1b1851ba96f6a03fdd7f7ed69ff5f3cf8f04b
Kyle Hughes <blinded93@gmail.com>
2019-10-11 20:01:58 -0700

    add bill scopes

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,16 @@
 class Bill < ApplicationRecord
   belongs_to :household
+
+  default_scope { order(due_date: :asc) }
+  scope :paid, -> { where(paid: true) }
+  scope :unpaid, -> { where.not(paid: true) }
+  scope :over_due, -> { unpaid.where('due_date < ?', Date.today) }
+  scope :soon, -> { where(due_date: Date.today..Date.today + 3.days) }
+  scope :this_month, -> {
+    where(due_date: Date.today.beginning_of_month..Date.today.end_of_month)
+  }
+  scope :next_month, -> {
+        where(due_date: Date.today.next_month.beginning_of_month..Date.today.next_month.end_of_month)
+  }
+
 end
