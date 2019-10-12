5671fa74774c2d9e4bd1c5fd225361713249ea0f
Kyle Hughes <blinded93@gmail.com>
2019-10-11 20:02:25 -0700

    add household associations and validations

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,3 +1,11 @@
 class Household < ApplicationRecord
   belongs_to :user
+
+  has_many :bills, dependent: :destroy
+  has_many :bills, dependent: :destroy
+  
+  serialize :members, Array
+  
+  validates :name, :city, :state, :zip_code, presence: true
+  validates :address1, presence: true, uniqueness: true
 end
